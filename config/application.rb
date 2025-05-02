c5023be09ba8f616a2497e93f0b73030b42f9058
tanbirramim <tanbirramim420@gmail.com>
2025-05-02 04:18:16 +0200

    Simplify Solid Queue configuration

--- config/application.rb
+++ config/application.rb
@@ -28,16 +28,5 @@ module MerchandiseDashboard
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Configure Solid Queue
-    config.active_job.queue_adapter = :solid_queue
-    
-    # Initialize Solid Queue configuration
-    config.after_initialize do
-      require 'solid_queue'
-      Rails.application.config.solid_queue.configure do |config|
-        config.concurrency = ENV.fetch("SOLID_QUEUE_CONCURRENCY", 5).to_i
-      end
-    end
   end
 end
--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,6 +1,2 @@
 # Configure Solid Queue
-Rails.application.config.solid_queue.configure do |config|
-  config.concurrency = ENV.fetch("SOLID_QUEUE_CONCURRENCY", 5).to_i
-  config.polling_interval = ENV.fetch("SOLID_QUEUE_POLLING_INTERVAL", 1).to_i
-  config.dispatcher_concurrency = ENV.fetch("SOLID_QUEUE_DISPATCHER_CONCURRENCY", 1).to_i
-end 
+Rails.application.config.active_job.queue_adapter = :solid_queue 
